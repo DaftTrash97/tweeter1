caed3cb8a594c9d70aed1a96e08335eb3acd7a78
Steph Nugs <steph.bowie1993@gmail.com>
2023-08-10 21:31:15 -0600

    created counter function useing jQuery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    console.log("I'm working!");
+  $(".new-tweet form textarea").on("input", function() {
+    let tweetLength = $(this).val().length;
+    let maxTweetLength = 140;
+    let remainingChars = maxTweetLength - tweetLength;
+
+    let counterElement = $(this).closest(".new-tweet").find(".counter");
+
+    counterElement.text(remainingChars);
   });
+});
