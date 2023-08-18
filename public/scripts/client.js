3d8bb970bf309f8fb5856a4fc442dd49dc3a259b
Steph Nugs <steph.bowie1993@gmail.com>
2023-08-18 17:59:33 -0600

    edited css for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,9 +3,16 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-  const createTweetElement = function (tweetData) {
-    const { user, content, created_at } = tweetData;
-    const $tweet = $(`
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+}
+
+const createTweetElement = function (tweetData) {
+  const { user, content, created_at } = tweetData;
+  const $tweet = $(`
   <article class="tweet">
     <header>
       <div class="profile-info">
@@ -24,26 +31,26 @@
     </footer>
   </article>
 `);
-    return $tweet;
+  return $tweet;
+};
+
+$(document).ready(function () {
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   };
 
-  $(document).ready(function () {
-    // Test / driver code (temporary). Eventually will get this from the server.
-    const tweetData = {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    };
-  
-    const $tweet = createTweetElement(tweetData);
-  
-    // Test / driver code (temporary)
-    console.log($tweet); // to see what it looks like
-    $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  });
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
