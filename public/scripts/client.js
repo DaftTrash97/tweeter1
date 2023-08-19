450491ac04c216c4eecfd7ba77ed9938f75836fd
Steph Nugs <steph.bowie1993@gmail.com>
2023-08-19 15:41:33 -0600

    added AJAX for sending a tweet to the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -63,24 +63,15 @@ const createTweetElement = function (tweetData) {
 
 $(document).ready(function () {
   renderTweets(data);
+
+  $('form').submit(function (event) {
+    event.preventDefault();
+
+    const formData = $(this).serialize();
+    console.log(formData);
+
+    $.post('/tweets', formData, function (response) {
+      console.log(response);
+    });
+  });
 });
-
-//   // Test / driver code (temporary). Eventually will get this from the server.
-//   const tweetData = {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   };
-
-//   const $tweet = createTweetElement(tweetData);
-
-//   // Test / driver code (temporary)
-//   console.log($tweet); // to see what it looks like
-//   $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-// });
