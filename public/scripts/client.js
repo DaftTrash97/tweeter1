0326c5d4cbed43f810ef6add820e6bb577817200
Steph Nugs <steph.bowie1993@gmail.com>
2023-08-24 15:03:08 -0600

    added comments to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,16 +1,16 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Client-side JS logic goes here
+// jQuery is already loaded
+// Reminder: Use (and do all your DOM work in) jQuery's document ready function
 
+// Render an array of tweet objects to the DOM
 const renderTweets = function (tweets) {
   for (const tweetData of tweets) {
     const $tweet = createTweetElement(tweetData);
     $('#tweets-container').prepend($tweet);
   }
-}
+};
 
+// Create and return a tweet element based on tweetData
 const createTweetElement = function (tweetData) {
   const { user, content, created_at } = tweetData;
   const $tweet = $(`
@@ -38,13 +38,15 @@ const createTweetElement = function (tweetData) {
   return $tweet;
 };
 
+// Load tweets from the server and render them on the page
 const loadTweets = function () {
   $.get('/tweets', function (tweets) {
     renderTweets(tweets);
   });
-}
+};
 
 $(document).ready(function () {
+  // Load existing tweets when the page is ready
   loadTweets();
 
   $('form').submit(function (event) {
@@ -53,21 +55,26 @@ $(document).ready(function () {
     const tweetText = $('#tweet-text').val();
 
     if (!tweetText.trim()) {
+      // Show an error message if the tweet text is empty
       $('#error-container').html('<i class="fa-solid fa-triangle-exclamation" style="color: #e41111;"></i> Tweet cannot be empty <i class="fa-solid fa-triangle-exclamation" style="color: #e41111;">').slideDown();
       return;
     }
 
     if (tweetText.length > 140) {
-      $('#error-container').html('<i class="fa-solid fa-triangle-exclamation" style="color: #e41111;"></i>Tweet is too long <i class="fa-solid fa-triangle-exclamation" style="color: #e41111;">').slideDown();
+      // Show an error message if the tweet is too long
+      $('#error-container').html('<i class="fa-solid fa-triangle-exclamation" style="color: #e41111;"></i> Tweet is too long <i class="fa-solid fa-triangle-exclamation" style="color: #e41111;">').slideDown();
       return;
     }
-    console.log($(this).serialize())
+
+    // Submit the new tweet, clear the textarea, and reset the character counter
+    console.log($(this).serialize());
     $.post('/tweets', $(this).serialize(), function (response) {
       loadTweets();
       $('#tweet-text').val('');
       $('.counter').text('140');
     });
-    
+
+    // Hide any existing error messages
     $('#error-container').slideUp();
   });
 });
